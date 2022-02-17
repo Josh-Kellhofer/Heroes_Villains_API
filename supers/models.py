1fa94ebb1d07e35229f388d477b248fa938c5175
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-17 11:19:55 -0800

    Created super_types app + Added Code For Foreignkey Into Super models.

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.forms import CharField
+from super_types.models import SuperType
 
 # Create your models here.
+
+class Supers(models.Model):
+  name = CharField(max_length=255)
+  alter_ego = CharField(max_length=255)
+  primary_ability = CharField(max_length=255)
+  secondary_ability = CharField(max_length=255)
+  catchphrase = CharField(max_length=255)
+  super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
