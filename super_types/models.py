a089efdd01b18cc9a15c92f60228fc83b3959b03
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-17 12:15:55 -0800

    Fixed Databases (added models) before fields to correctly migrate and populate fields in msql.

--- super_types/models.py
+++ super_types/models.py
@@ -4,4 +4,4 @@ from django.forms import CharField
 # Create your models here.
 
 class SuperType(models.Model):
-    type = CharField(max_length=255)
+    type = models.CharField(max_length=255, default = "")
--- supers/models.py
+++ supers/models.py
@@ -5,9 +5,9 @@ from super_types.models import SuperType
 # Create your models here.
 
 class Supers(models.Model):
-  name = CharField(max_length=255)
-  alter_ego = CharField(max_length=255)
-  primary_ability = CharField(max_length=255)
-  secondary_ability = CharField(max_length=255)
-  catchphrase = CharField(max_length=255)
-  super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+  name = models.CharField(max_length=255, default = "")
+  alter_ego = models.CharField(max_length=255, default = "")
+  primary_ability = models.CharField(max_length=255, default = "")
+  secondary_ability = models.CharField(max_length=255, default = "")
+  catchphrase = models.CharField(max_length=255, default = "")
+  super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE, default = "")
