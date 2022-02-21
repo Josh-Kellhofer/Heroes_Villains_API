da4bdd23dbf92a3b0e9f2ddb33dde1a35fa86635
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-21 13:19:03 -0800

    Created battle App.

---
+++ battle/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+
+class Battle(models.Model):
+  super_one = models.ForeignKey
+  super_two = models.ForeignKey
+  battle_date = 
+  
